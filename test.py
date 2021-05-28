6669b5bbba3179cd680052227b8912e37ab4f9b7
Felipe Cabrera <fecabrera@protonmail.com>
2021-05-27 23:07:05 -0400

    Test updated for the new DateEncoder

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 from mongomapper import Schema, Reference
+from datetime import date
 
 class Person(Schema):
   __collection_name__ = 'people'
   
   name: str
+  birthday: date
 
 class Car(Schema):
   __collection_name__ = 'cars'
@@ -11,7 +13,7 @@ class Car(Schema):
   plate: str
   owner: Reference(Person)
 
-person = Person.create(name="Felipe")
+person = Person.create(name="Felipe", birthday="1995-12-02")
 car = Car.create(plate="ABCD10", owner=person)
 
 print(person.reference)
