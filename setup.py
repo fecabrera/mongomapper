1c2f93085cbeba2fc770ec2a66e781864847e833
Felipe Cabrera <fecabrera@protonmail.com>
2021-05-31 09:08:28 -0400

    Minor revision

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   description='Mongo ODM for Python',
   long_description=long_description,
   long_description_content_type="text/markdown",
-  version='1.0.0',
+  version='1.0.1',
   license='MIT',
   author='Felipe Cabrera',
   author_email='fecabrera@protonmail.com',
